8c89d8a443e02219512b00c51018bcb1a503bcf9
Meinhard Kissich <privat@meinhard-kissich.at>
2026-08-06 11:05:51 +0200

    Add original xorshift

---
+++ macros/heichips26_digital_project/rtl/xorshift32.v
@@ -0,0 +1,34 @@
+module xorshift32 (
+    input  wire        clk,
+    input  wire        reset, // Positive reset
+    output wire  [31:0] out
+);
+    reg [31:0] out_reg;
+    initial out_reg = 32'd314159265;
+    assign out = out_reg;
+    // Wires for the intermediate combinational logic steps
+    wire [31:0] step1;
+    wire [31:0] step2;
+    wire [31:0] next_out;
+
+    // Combinational logic: Executing the shifts and XORs in a single cycle
+    assign step1    = out   ^ (out_reg   << 13);
+    assign step2    = step1 ^ (step1 >> 17);
+    assign next_out = step2 ^ (step2 <<  5);
+
+    // Sequential logic: Updating the state
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            // Apply the initial seed value specified in the C code
+            out_reg <= 32'd314159265; 
+        end else begin
+            // Clock in the new generated pseudo-random number
+            if(next_out == 32'd0) begin
+            out_reg <= 32'd314159265; 
+            end else begin
+            out_reg <= next_out;
+            end
+        end
+    end
+
+endmodule
