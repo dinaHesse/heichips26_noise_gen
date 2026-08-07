bee626674ad18107e2ad3276e0912d7b6387fc2c
dinaHesse <sfnersuperkeeper@web.de>
2026-08-07 12:43:53 +0200

    second config_ro try - still weird behaviour

--- macros/heichips26_digital_project/macros/config_ro/rtl/config_ro.sv
+++ macros/heichips26_digital_project/macros/config_ro/rtl/config_ro.sv
@@ -6,8 +6,8 @@
 
 module config_ro
 #(
-  parameter int unsigned NUM_STAGES = 9, //64+1
-  parameter int unsigned SKIP_STAGES = 2
+  parameter int unsigned NUM_STAGES = 64+1,
+  parameter int unsigned SKIP_STAGES = 16
 )(
   input logic                   enable,
   input logic [(NUM_STAGES/SKIP_STAGES)-1:0]  stage_en,
@@ -16,42 +16,43 @@ module config_ro
 );
 
     // RO implementation
+    wire [NUM_STAGES:0] sig_inverter;
     wire [NUM_STAGES-1:0] inverter_out;
     wire [NUM_STAGES-1:0] stage_out;
     wire [NUM_STAGES-1:0] load_en;
 
-    assign osc = inverter_out[NUM_STAGES-1] & enable;// buffer_out[63];// buffer for testing
+ //   assign osc = stage_out[NUM_STAGES-1] & enable;// buffer_out[63];// buffer for testing
 
-  genvar i;
-  generate
-    for (i = 0; i < NUM_STAGES; i = i + 1)
-    begin
-      if(i == 0) begin
-        (* keep *) sg13cmos5l_inv_1 inverter_inst (
-          .A(enable ? inverter_out[NUM_STAGES-1] : 1'b0), // Last stage feedback if enabled
-          .Y(inverter_out[i]) // First stage output
-        );
-        assign stage_out[i] = inverter_out[i];
-      end
-      else begin
-        if (i % SKIP_STAGES == 0)
-        begin
-          (* keep *) sg13cmos5l_inv_1 inverter_inst (
-            .A(stage_out[i-1]), // Previous stage output
-            .Y(inverter_out[i]) // Current stage output
-          );
-          assign stage_out[i] = inverter_out[i];
-        end
-        else begin
-          (* keep *) sg13cmos5l_inv_16 inverter_inst (
-            .A(stage_out[i-1]), // Previous stage output
-            .Y(inverter_out[i]) // Current stage output
-          );
-          assign stage_out[i] = stage_en[i/SKIP_STAGES] ? inverter_out[i] : stage_out[i-SKIP_STAGES];
-        end
-      end
-    end
-  endgenerate
+  // genvar i;
+  // generate
+  //   for (i = 0; i < NUM_STAGES; i = i + 1)
+  //   begin
+  //     if(i == 0) begin
+  //       (* keep *) sg13cmos5l_inv_1 inverter_inst (
+  //         .A(enable ? stage_out[NUM_STAGES-1] : 1'b0), // Last stage feedback if enabled
+  //         .Y(inverter_out[i]) // First stage output
+  //       );
+  //       assign stage_out[i] = inverter_out[i];
+  //     end
+  //     else begin
+  //       if (i % SKIP_STAGES == 0)
+  //       begin
+  //         (* keep *) sg13cmos5l_inv_1 inverter_inst (
+  //           .A(stage_out[i-1]), // Previous stage output
+  //           .Y(inverter_out[i]) // Current stage output
+  //         );
+  //         assign stage_out[i] = stage_en[(i/SKIP_STAGES)-1] ? inverter_out[i] : stage_out[i-SKIP_STAGES];
+  //       end
+  //       else begin
+  //         (* keep *) sg13cmos5l_inv_1 inverter_inst (
+  //           .A(stage_out[i-1]), // Previous stage output
+  //           .Y(inverter_out[i]) // Current stage output
+  //         );
+  //         assign stage_out[i] = inverter_out[i];
+  //       end
+  //     end
+  //   end
+  // endgenerate
 
 // wire[8-1:0]  buffer_out;
 //       (* keep *)sg13cmos5l_buf_1 buffer
@@ -72,21 +73,41 @@ module config_ro
 //   endgenerate
 
 
-  assign  load_en[NUM_STAGES-1:0] = stage_out[NUM_STAGES-1:0];
+  genvar i;
+  generate
+    for (i = 0; i < NUM_STAGES; i = i + 1)
+    begin
+          (* keep *) sg13cmos5l_inv_1 inverter_inst (
+            .A(sig_inverter[i]), // Previous stage output
+            .Y(sig_inverter[i+1]) // Current stage output
+          );
+    end
+  endgenerate
+
+  assign sig_inverter[0] = stage_en[0] ? sig_inverter[1] :
+                            stage_en[1]? sig_inverter[1+SKIP_STAGES] :
+                             stage_en[2]? sig_inverter[1+2*SKIP_STAGES] :
+                              stage_en[3]? sig_inverter[1+3*SKIP_STAGES] :
+                                sig_inverter[NUM_STAGES-1] ;
+
+  assign osc = sig_inverter[0];//stage_out[NUM_STAGES-1] & enable;// buffer_out[63];// buffer for testing
 
 
-// connect a load to each inverter output
-  // generate
-  //   for (i = 0; i < NUM_STAGES; i = i + 1)
-  //   begin
-  //     (* keep *)load_parallel #(.WIDTH(2)) load
-  //       (
-  //           .input_ro(load_en[i]),
-  //           .enable_n(8'd0),
-  //           .output_buffer()
-  //       );
-  //   end
-  // endgenerate
+  assign  load_en[NUM_STAGES-1:0] = sig_inverter[NUM_STAGES:1];
+
+
+//connect a load to each inverter output
+  generate
+    for (i = 0; i < NUM_STAGES; i = i + 4)
+    begin
+      (* keep *)load_parallel #(.WIDTH(1)) load
+        (
+            .input_ro(load_en[i]),
+            .enable_n(8'd0),
+            .output_buffer()
+        );
+    end
+  endgenerate
 
 
 
--- macros/heichips26_digital_project/macros/config_ro/testbenches/xschem/config_ro_tb_tran.sch
+++ macros/heichips26_digital_project/macros/config_ro/testbenches/xschem/config_ro_tb_tran.sch
@@ -13,8 +13,8 @@ ypos2=1.439978
 divy=5
 subdivy=1
 unity=1
-x1=1.0066735e-09
-x2=5e-09
+x1=-9.2105306e-09
+x2=-5.2172041e-09
 divx=5
 subdivx=1
 xlabmag=1.0
@@ -38,8 +38,8 @@ ypos2=2
 divy=5
 subdivy=1
 unity=1
-x1=1.0066735e-09
-x2=5e-09
+x1=-9.2105306e-09
+x2=-5.2172041e-09
 divx=5
 subdivx=1
 xlabmag=1.0
@@ -74,8 +74,8 @@ N 500 -590 500 -520 {lab=VDD
 spice_ignore=true}
 N 540 -500 540 -430 {lab=GND
 spice_ignore=true}
-N 890 -460 1040 -460 {lab=VDD,VDD,VDD,VDD bus=true}
-N 890 -470 890 -460 {lab=VDD,VDD,VDD,VDD}
+N 890 -460 1040 -460 {lab=VDD,VDD,GND,GND bus=true}
+N 890 -470 890 -460 {lab=VDD,VDD,GND,GND}
 N 440 -510 630 -510 {lab=#net1 bus=true
 spice_ignore=true}
 C {devices/vsource.sym} 120 -650 0 0 {name=VDD value="\{VDD\}"}
@@ -105,7 +105,7 @@ set num_threads=8
 *save all
 
 * User Constants
-let tstop = 8n
+let tstop = 5n
 let tstep = 100p
 
 * Operating Point Analysis
@@ -162,7 +162,7 @@ value=0.1f
 footprint=1206
 device="ceramic capacitor"}
 C {devices/gnd.sym} 1200 -320 0 0 {name=l1 lab=GND}
-C {devices/lab_wire.sym} 890 -470 0 1 {name=p14 sig_type=std_logic lab=VDD,VDD,VDD,VDD}
+C {devices/lab_wire.sym} 890 -470 0 1 {name=p14 sig_type=std_logic lab=GND,GND,GND,GND}
 C {bus_tap.sym} 440 -510 0 0 {name=l4 lab=0
 spice_ignore=true}
 C {bus_tap.sym} 450 -510 2 1 {name=l5 lab=1
