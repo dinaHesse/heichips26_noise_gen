19a25085a6d7aec175421c1b12148a6a469a9403
Meinhard Kissich <privat@meinhard-kissich.at>
2026-08-21 15:24:46 +0200

    Update macro configs, implement d_ro_tapped

--- macros/heichips26_digital_project/macros/d_ro_tapped/rtl/d_ro_tapped.sv
+++ macros/heichips26_digital_project/macros/d_ro_tapped/rtl/d_ro_tapped.sv
@@ -4,21 +4,32 @@
 /* verilator lint_off UNOPTFLAT */
 `default_nettype none
 
-module d_ro_tapped (
-  input  logic enable,
-  output logic osc
+module d_ro_tapped #(
+  parameter FSEL_BITS = 8
+)(
+  input  logic                  enable,
+  output logic                  osc,
+  input  logic [FSEL_BITS-1:0]  fsel_i
 );
 
-localparam NUM_STAGES = 149;
+localparam NUM_STAGES_MIN = 3;
 
-wire [NUM_STAGES:0] inv_wire;
+localparam NUM_CODES      = 1 << FSEL_BITS;
+localparam NUM_STAGES_MAX = NUM_STAGES_MIN + 2*(NUM_CODES-1);
 
-assign inv_wire[0] = inv_wire[NUM_STAGES] & enable;
-assign osc = inv_wire[2];
+wire [NUM_STAGES_MAX:0]             inv_wire;
+wire [$clog2(NUM_STAGES_MAX+1)-1:0] tap_idx;
+wire                                feedback;
+
+assign tap_idx  = NUM_STAGES_MIN + {fsel_i, 1'b0};
+assign feedback = inv_wire[tap_idx];
+
+assign inv_wire[0] = feedback & enable;
+assign osc         = inv_wire[2];
 
 genvar i;
 generate
-  for (i = 0; i < NUM_STAGES; i = i + 1) begin
+  for (i = 0; i < NUM_STAGES_MAX; i = i + 1) begin
     sg13cmos5l_inv_1 i_inv (
       .A( inv_wire[i]   ),
       .Y( inv_wire[i+1] )
