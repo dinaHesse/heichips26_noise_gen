17535181280ab39a52daa5c670f114b09e530ac0
Meinhard Kissich <privat@meinhard-kissich.at>
2026-08-24 08:58:27 +0200

    Init f_load_sweep

---
+++ macros/heichips26_digital_project/macros/f_load_sweep/rtl/f_load_sweep.sv
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2026 XXX
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+
+/* verilator lint_off UNOPTFLAT */
+`default_nettype none
+
+module f_load_sweep (
+  input  logic enable,
+  output logic osc
+);
+
+// TODO
+
+endmodule
+
+
+`default_nettype wire
+/* verilator lint_on UNOPTFLAT */
