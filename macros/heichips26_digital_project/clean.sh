15afb3285edb1fa97d37ec8a4a0edf10f68921f3
Meinhard Kissich <privat@meinhard-kissich.at>
2026-08-25 10:35:45 +0200

    Adapt readme, small script changes

--- macros/heichips26_digital_project/clean.sh
+++ macros/heichips26_digital_project/clean.sh
@@ -7,3 +7,4 @@ cd macros/ctrl && make clean && cd ../..
 cd macros/e_load_uniform && make clean && cd ../..
 cd macros/f_load_binary && make clean && cd ../..
 cd macros/g_glitch && make clean && cd ../..
+make clean
--- macros/heichips26_digital_project/run.sh
+++ macros/heichips26_digital_project/run.sh
@@ -1,9 +1,10 @@
 
-cd macros/a_ro && make librelane && make copy-final && cd ../..
-cd macros/b_ro && make librelane && make copy-final && cd ../..
-cd macros/c_ro && make librelane && make copy-final && cd ../..
-cd macros/d_ro_tapped && make librelane && make copy-final && cd ../..
-cd macros/ctrl && make librelane && make copy-final && cd ../..
-cd macros/e_load_uniform && make librelane && make copy-final && cd ../..
-cd macros/f_load_binary && make librelane && make copy-final && cd ../..
-cd macros/g_glitch && make librelane && make copy-final && cd ../..
+#cd macros/a_ro && make build-top && cd ../..
+#cd macros/b_ro && make build-top && cd ../..
+#cd macros/c_ro && make build-top && cd ../..
+#cd macros/d_ro_tapped && make build-top && cd ../..
+cd macros/ctrl && make build-top && cd ../..
+cd macros/e_load_uniform && make build-top && cd ../..
+cd macros/f_load_binary && make build-top && cd ../..
+cd macros/g_glitch && make build-top && cd ../..
+make build-top
