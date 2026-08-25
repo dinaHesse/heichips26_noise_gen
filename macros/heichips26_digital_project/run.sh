8e6134b3d8acc258bfd45bea9ce07c75d3356a65
Meinhard Kissich <privat@meinhard-kissich.at>
2026-08-25 11:47:21 +0200

    Fixes

--- macros/heichips26_digital_project/run.sh
+++ macros/heichips26_digital_project/run.sh
@@ -1,8 +1,8 @@
 
-#cd macros/a_ro && make build-top && cd ../..
-#cd macros/b_ro && make build-top && cd ../..
-#cd macros/c_ro && make build-top && cd ../..
-#cd macros/d_ro_tapped && make build-top && cd ../..
+cd macros/a_ro && make build-top && cd ../..
+cd macros/b_ro && make build-top && cd ../..
+cd macros/c_ro && make build-top && cd ../..
+cd macros/d_ro_tapped && make build-top && cd ../..
 cd macros/ctrl && make build-top && cd ../..
 cd macros/e_load_uniform && make build-top && cd ../..
 cd macros/f_load_binary && make build-top && cd ../..
